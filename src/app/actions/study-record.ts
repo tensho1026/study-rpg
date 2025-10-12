8259ef231fdecffeb1b17b44a03884b862c30391
tensho1026 <tensho1026@icloud.com>
2025-10-13 06:08:14 +0900

    fix:where sentence

--- src/app/actions/study-record.ts
+++ src/app/actions/study-record.ts
@@ -7,27 +7,28 @@ import { getServerSession } from "next-auth";
 export const saveStudy = async (minutes: number) => {
   const session = await getServerSession(authOptions);
   const today = new Date();
-  
-  // 時間を０にする
-  today.setHours(0, 0, 0, 0); 
 
-  if(!session) {
-    return
+  // 時間を０にする
+  today.setHours(0, 0, 0, 0);
+
+  if (!session) {
+    return;
   }
 
   await prisma.studyRecord.upsert({
-  where:{
-    userId:session?.user.id,
-    Date:today,
-  },
-  update:{
-    minutes: { increment: minutes },
-
-  },
-  create:{
-    userId:session?.user.id,
-    minutes:minutes,
-    Date:today
-  }
-  })
+    where: {
+      userId_Date: {
+        userId: session.user.id,
+        Date: today,
+      },
+    },
+    update: {
+      minutes: { increment: minutes },
+    },
+    create: {
+      userId: session?.user.id,
+      minutes: minutes,
+      Date: today,
+    },
+  });
 };
