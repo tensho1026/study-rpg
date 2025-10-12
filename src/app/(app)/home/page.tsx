46773348f0cfac818fc298897e6decd0a97f454f
tensho1026 <tensho1026@icloud.com>
2025-10-13 06:07:51 +0900

    add:console about homedata

--- src/app/(app)/home/page.tsx
+++ src/app/(app)/home/page.tsx
@@ -1,16 +1,27 @@
 "use client";
 
-
 import { StatusWindow } from "@/components/status-window";
 import { EquipmentWindow } from "@/components/equipment-window";
 import { StudyTimer } from "@/components/study-timer";
 import { MessageBox } from "@/components/message-box";
 import { useSession } from "next-auth/react";
+import { getHomeData } from "@/app/actions/getHomeData";
+import { useEffect } from "react";
 
 export default function StudyQuestPage() {
   const { data } = useSession();
   console.log(data);
 
+  useEffect(() => {
+    const fetchdata = async () => {
+  const homedata = await getHomeData();
+    console.log(homedata,'ホームデータ');
+    }
+
+    fetchdata()
+  }, []);
+  // const datas = getHomeData();
+  // console.log(datas, "ホームデータ");
 
   return (
     <main className="min-h-screen bg-background p-4 md:p-8">
@@ -25,26 +36,20 @@ export default function StudyQuestPage() {
           </p>
         </div>
 
-         <MessageBox/>
+        <MessageBox />
 
         {/* Main Game Area */}
         <div className="grid grid-cols-1 lg:grid-cols-3 gap-4">
           {/* Left Column - Status */}
           <div className="space-y-4">
-            <StatusWindow
-     
-            />
+            <StatusWindow />
 
-            <EquipmentWindow
-  
-            />
+            <EquipmentWindow />
           </div>
 
           {/* Center Column - Timer & Character */}
           <div className="lg:col-span-2 space-y-4">
             <StudyTimer />
-
-           
 
             {/* Character Display */}
           </div>
