ece205bab432e5d3eeb44c54063333eeec55533e
tensho1026 <tensho1026@icloud.com>
2025-10-25 11:02:53 +0900

    装備変更ロジック

---
+++ src/app/actions/equipment/equipItem.ts
@@ -0,0 +1,43 @@
+"use server";
+
+import { authOptions } from "@/lib/auth";
+import { prisma } from "@/lib/prisma";
+import { getServerSession } from "next-auth";
+import { EquipmentType } from "@prisma/client";
+
+export default async function equipItem(equipmentId: string, type: string) {
+  const session = await getServerSession(authOptions);
+  if (!session) return;
+  const userId = session.user.id;
+
+  // つけている装備検索
+  const draftedItemId = await prisma.equipment.findFirst({
+    where: {
+      userId: session.user.id,
+      isDraft: true,
+      mstEquipment: {
+        type: type as EquipmentType,
+      },
+    },
+    select: {
+      equipmentId: true,
+    },
+  });
+
+  if (!draftedItemId) return;
+
+  // つけていた装備を外す
+  await prisma.equipment.update({
+    where: { userId_equipmentId: { userId, equipmentId } },
+    data: {
+      isDraft: !true,
+    },
+  });
+
+  await prisma.equipment.update({
+    where: { userId_equipmentId: { userId, equipmentId } },
+    data: {
+      isDraft: true,
+    },
+  });
+}
