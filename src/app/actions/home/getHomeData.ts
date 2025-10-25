bf119c5eadf2050bea02075f246caa882c49356b
tensho1026 <tensho1026@icloud.com>
2025-10-25 09:14:54 +0900

    分離

--- src/app/actions/home/getHomeData.ts
+++ src/app/actions/home/getHomeData.ts
@@ -1,42 +1,16 @@
 "use server";
 
 import { authOptions } from "@/lib/auth";
-import { prisma } from "@/lib/prisma";
-import getToday from "@/utils/getToday";
+import getOrCreateTodayStudyRecord from "@/lib/get/getTodayStudyRecord";
+import { getOrCreateUserStatusFunction } from "@/lib/get/getUserStatus";
 import { getServerSession } from "next-auth";
 
 export const getHomeData = async () => {
   const session = await getServerSession(authOptions);
   if (!session) return;
 
-  const today = getToday();
+  const userStatus = getOrCreateUserStatusFunction(session.user.id);
 
-  let userStatus = await prisma.userStatus.findFirst({
-    where: { userId: session?.user.id },
-  });
-
-  if (!userStatus && session) {
-    userStatus = await prisma.userStatus.create({
-      data: {
-        userId: session?.user.id,
-      },
-    });
-  }
-
-  let todayStudyRecord = await prisma.studyRecord.findFirst({
-    where: {
-      userId: session?.user.id,
-      Date: today,
-    },
-  });
-  if (!todayStudyRecord) {
-    todayStudyRecord = await prisma.studyRecord.create({
-      data: {
-        userId: session?.user.id,
-        Date: today,
-        minutes: 0,
-      },
-    });
-  }
+  const todayStudyRecord = getOrCreateTodayStudyRecord(session.user.id);
   return { userStatus, todayStudyRecord };
 };
