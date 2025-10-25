7414415a197d43038400b8344775eb7366f51d20
tensho1026 <tensho1026@icloud.com>
2025-10-25 16:07:10 +0900

    type分離

--- src/app/actions/equipment/equipItem.ts
+++ src/app/actions/equipment/equipItem.ts
@@ -24,15 +24,20 @@ export default async function equipItem(equipmentId: string, type: string) {
     },
   });
 
-  if (!draftedItemId) return;
-
-  // つけていた装備を外す
-  await prisma.equipment.update({
-    where: { userId_equipmentId: { userId, equipmentId } },
-    data: {
-      isDraft: !true,
-    },
-  });
+  if (draftedItemId) {
+    // つけていた装備を外す
+    await prisma.equipment.update({
+      where: {
+        userId_equipmentId: {
+          userId: session.user.id,
+          equipmentId: draftedItemId.equipmentId,
+        },
+      },
+      data: {
+        isDraft: !true,
+      },
+    });
+  }
 
   await prisma.equipment.update({
     where: { userId_equipmentId: { userId, equipmentId } },
