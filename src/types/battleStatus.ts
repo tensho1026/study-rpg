8ac44fb32a0d542c2920416780b7e0836390b412
tensho1026 <tensho1026@icloud.com>
2025-10-26 15:16:52 +0900

    型修正

--- src/types/battleStatus.ts
+++ src/types/battleStatus.ts
@@ -1,30 +1,19 @@
-import { EquipmentType } from "@prisma/client";
+import { Prisma } from "@prisma/client";
 
-export type BattleStatusType = {
-  id: string;
-  userId: string;
-  maxHp: number;
-  hp: number;
-  attack: number;
-  defense: number;
-  level: number;
-  exp: number;
-  user: {
-    equipments: {
-      id: string;
-      equipmentId: string;
-      isDraft: boolean;
-      createdAt: string; 
-      updatedAt: string;
-      mstEquipment: {
-        id: string;
-        name: string;
-        type: EquipmentType;
-        attack: number | null;
-        defense: number | null;
-        price: number;
-        description: string;
+export type BattleStatusType = Prisma.BattleStatusGetPayload<{
+  include: {
+    user: {
+      select: {
+        name: true;
+        equipments: {
+          where: {
+            isDraft: true;
+          };
+          include: {
+            mstEquipment: true;
+          };
+        };
       };
-    }[];
+    };
   };
-};
+}>;
