507965aaac700d68109769cfc86cc90334e5e094
tensho1026 <tensho1026@icloud.com>
2025-11-05 22:52:17 +0900

    shopのuseeffect削除

--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -1,5 +1,13 @@
 import Shop from "@/components/shop/Shop";
+import getEquipmentData from "../actions/shop/getShopData";
 
-export default function ShopPage() {
-  return <Shop />;
+export default async function ShopPage() {
+  const data = await getEquipmentData();
+  return (
+    <Shop
+      shopEquipments={data?.equipments ?? []}
+      coin={data?.userCoins ?? 0}
+      userEquipment={data?.userEquipments ?? []}
+    />
+  );
 }
