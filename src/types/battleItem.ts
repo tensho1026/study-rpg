7b8e187dfb7d73bebfef38b4e2b931e259c53222
tensho1026 <tensho1026@icloud.com>
2025-11-06 23:19:30 +0900

    型修正

--- src/types/battleItem.ts
+++ src/types/battleItem.ts
@@ -3,7 +3,8 @@ export type BattleItem = {
   name: string;
   description: string;
   type: "heal" | "attack" | "buff" | "material" | "key";
-  heal?: { hp?: number; mp?: number }; // ← mpをオプションとして定義
+  healHp?: number | null;
+  healMp?: number | null;
   price: number | null;
   rarity: number | null;
   quantity?: number;
