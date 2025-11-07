785b41857c8deace3039d15fb3188ef990e43fb5
tensho1026 <tensho1026@icloud.com>
2025-11-07 23:44:19 +0900

    a

--- src/lib/battle/getEnemy.ts
+++ src/lib/battle/getEnemy.ts
@@ -1,19 +1,25 @@
+"use server";
 import { prisma } from "../prisma";
 
-export default async function getEnemy() {
-  const enemyCount = await prisma.mstEnemies.count();
-  if (enemyCount === 0) return null;
-
-  const randomIndex = String(Math.floor(Math.random() * enemyCount) + 1);
-
-  const data = await prisma.mstEnemies.findFirst({
+export default async function getEnemy(type: string) {
+  const enemys = await prisma.mstEnemies.findMany({
     where: {
-      id: randomIndex,
-    },
-    include: {
-      dropItem: true,
+      mapId: type,
     },
   });
 
-  return data;
+  const randomEnemy = enemys[Math.floor(Math.random() * enemys.length)];
+
+  // const randomIndex = String(Math.floor(Math.random() * enemyCount) + 1);
+
+  // const data = await prisma.mstEnemies.findFirst({
+  //   where: {
+  //     id: randomIndex,
+  //   },
+  //   include: {
+  //     dropItem: true,
+  //   },
+  // });
+
+  return randomEnemy;
 }
