b548d4ab6419d4f0af0fa3ff4716acc3336f1689
tensho1026 <tensho1026@icloud.com>
2025-11-09 01:57:22 +0900

    upsertで完結

--- src/lib/study/getTodayStudyRecord.ts
+++ src/lib/study/getTodayStudyRecord.ts
@@ -1,28 +1,22 @@
 import getToday from "@/utils/getToday";
 import { prisma } from "../prisma";
 
-async function getTodayStudyRecord(userId: string) {
-  const today = getToday();
-
-  return await prisma.studyRecord.findFirst({
-    where: {
-      userId: userId,
-      Date: today,
-    },
-  });
-}
-
 export default async function getOrCreateTodayStudyRecord(userId: string) {
   const today = getToday();
-  let record = await getTodayStudyRecord(userId);
-  if (!record) {
-    record = await prisma.studyRecord.create({
-      data: {
-        userId: userId,
+  const record = await prisma.studyRecord.upsert({
+    where: {
+      userId_Date: {
+        userId,
         Date: today,
-        minutes: 0,
       },
-    });
-  }
-  return record
+    },
+    update: {},
+    create: {
+      userId: userId,
+      Date: today,
+      minutes: 0,
+    },
+  });
+
+  return record;
 }
