f9d5f5d7211c7d2db88fda6d720a4b026772d272
tensho1026 <tensho1026@icloud.com>
2025-11-09 01:21:47 +0900

    未ログイン改善

--- src/lib/battle/calcUserBattleStatus.ts
+++ src/lib/battle/calcUserBattleStatus.ts
@@ -1,24 +1,29 @@
-// lib/battle/calcUserBattleStatus.ts
+import { BattleStatusType } from "@/types/battleStatus";
+
+type BattleEquipment = {
+  mstEquipment: {
+    type: "weapon" | "armor";
+    attack?: number;
+    defense?: number;
+  };
+};
 
 export default function calcUserBattleStatus(
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  battleStatus: any,
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  userEquipments: any
+  battleStatus: BattleStatusType | null,
+
+  userEquipments: BattleEquipment[] | null
 ) {
   if (!battleStatus) return { attack: 0, defense: 0 };
 
-  const userWeapon = userEquipments?.filter(
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    (equip: any) => equip.mstEquipment.type === "weapon"
+  const userWeapon = userEquipments?.find(
+    (equip) => equip.mstEquipment.type === "weapon"
   );
-  const userArmor = userEquipments?.filter(
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    (equip: any) => equip.mstEquipment.type === "armor"
+  const userArmor = userEquipments?.find(
+    (equip) => equip.mstEquipment.type === "armor"
   );
 
-  const userWeaponAttack = userWeapon?.[0]?.mstEquipment?.attack ?? 0;
-  const userArmorDefense = userArmor?.[0]?.mstEquipment?.defense ?? 0;
+  const userWeaponAttack = userWeapon?.mstEquipment?.attack ?? 0;
+  const userArmorDefense = userArmor?.mstEquipment?.defense ?? 0;
 
   return {
     attack: (battleStatus.attack ?? 0) + userWeaponAttack,
